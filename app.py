f0e1bc47036ce29dcf217b483810c7373c429873
marceloeihara <43140663+marceloeihara@users.noreply.github.com>
2025-02-11 21:41:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-return "Aplicação Flask rodando com
-sucesso!"
+  return "Aplicação Flask rodando com sucesso!"
 if __name__ == "__main__":
-app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000)
